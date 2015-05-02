d925b826a667cb3eb3c911e9895b9ae4aecc87f8
Leland Richardson <leland@tech.pro>
2015-05-02 16:50:16 -0700

    reformatting, added open route

--- components/NavButton.js
+++ components/NavButton.js
@@ -3,48 +3,47 @@
 var React = require('react-native');
 
 var {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-} = React;
-
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    } = React;
 
 var NavButton = React.createClass({
 
-  onPress: function() {
-    this.props.onPress();
-  },
+    onPress: function () {
+        this.props.onPress();
+    },
 
-  render() {
-    var backButton;
+    render () {
+        var backButton;
 
-    if (this.props.backButtonComponent) {
-      var BackButton = this.props.backButtonComponent;
-      backButton = <View><BackButton /></View>
-    } else {
-      backButton = <Text style={styles.navbarText}>Back</Text>
-    }
+        if (this.props.backButtonComponent) {
+            var BackButton = this.props.backButtonComponent;
+            backButton = <View>
+                <BackButton />
+            </View>
+        } else {
+            backButton = <Text style={styles.navbarText}>Back</Text>
+        }
 
-    return (
-      <TouchableHighlight onPress={this.onPress} underlayColor="transparent">
+        return (
+            <TouchableHighlight onPress={this.onPress} underlayColor="transparent">
         {backButton}
-      </TouchableHighlight>
-    )
-  }
+            </TouchableHighlight>
+        )
+    }
 });
-
 
 var styles = StyleSheet.create({
-  navbarText: {
-    color: 'white',
-    fontSize: 16,
-    margin: 10,
-    fontWeight: '600',
-    textAlign: 'center',
-    alignItems: 'center',
-  }
+    navbarText: {
+        color: 'white',
+        fontSize: 16,
+        margin: 10,
+        fontWeight: '600',
+        textAlign: 'center',
+        alignItems: 'center',
+    }
 });
-
 
 module.exports = NavButton;
